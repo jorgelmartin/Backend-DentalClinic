23a6822612d27ef0af19214637fba137ba44bbfd
Jorge Martin <jorgemctin@gmail.com>
2023-06-07 00:43:09 +0200

    FEAT: router file to database

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express'); 
-// const db = require('./db');
+const db = require('./db');
 // const router = require('./router');
 // const auth = require('./middlewares/verifyToken');
 // const authController = require('./controllers/authController');
---
+++ router.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const authRoutes = require('./views/authRoutes');
+const roleRoutes = require('./views/roleRoutes')
+
+router.use('/auth', authRoutes);
+router.use('/role', roleRoutes);
+
+module.exports = router;
