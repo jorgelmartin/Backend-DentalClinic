012f019a2520877bbc4ae3749e79eff009d839f3
Jorge Martin <jorgemctin@gmail.com>
2023-06-06 23:37:08 +0200

    FEAT: Express server runing

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express'); 
+// const db = require('./db');
+// const router = require('./router');
+// const auth = require('./middlewares/verifyToken');
+// const authController = require('./controllers/authController');
+
+const app = express();
+
+const PORT = 4000;
+
+
+app.get('/health', (req, res) => {
+    return res.send('healthy');
+});
+
+app.use(express.json());
+
+// db.then(() =>
+//     {
+//         app.listen(PORT, () => {
+//             console.log('Server is runing on PORT: ' + PORT);
+//         })
+//     }
+// ).catch((error) => {
+//     console.log('error starting ar server', error.message);
+// });
