c3d477decab1fa4232689b4d5cf579ec777c395b
Jorge Martin <jorgemctin@gmail.com>
2023-09-15 18:03:20 +0200

    Fix: cors

--- index.js
+++ index.js
@@ -5,20 +5,23 @@ const db = require('./db');
 const router = require('./router');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'https://react-dentalclinic.vercel.app', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+}));
 app.use(express.static('public'));
 //PORT
-const PORT = 4000; 
+const PORT = 4000;
 app.use(express.json());
 app.use(cors());
 app.use(router);
 
-db.then(() =>
-    {
-        app.listen(PORT, () => {
-            console.log('Server is running on port: ' + PORT);
-        })
-    }
+db.then(() => {
+    app.listen(PORT, () => {
+        console.log('Server is running on port: ' + PORT);
+    })
+}
 ).catch((error) => {
     console.error('Error starting server', error.message);
 })
