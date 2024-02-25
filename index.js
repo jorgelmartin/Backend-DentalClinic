afc018f5e3c86d362e324ee7f858c9f80eb69a25
Jorge Martin <jorgemctin@gmail.com>
2024-02-25 22:07:04 +0100

    Fix: deploy backend and DB

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ const router = require('./router');
 const app = express();
 app.use(cors());
 app.use(express.static('public'));
+
 //PORT
 const PORT = 4000; 
 app.use(express.json());
-app.use(cors());
+
 app.use(router);
 
 db.then(() =>
